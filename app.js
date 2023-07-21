13d153c2f2dd3d08d9a5c6af04383c43a633c79a
Joulence <4erviken@gmail.com>
2023-07-21 16:30:18 +0300

    Added ServerCall

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const basicTask = Symbol("basicTask");
+const serverCall = Symbol("serverCall");
 
 class TestWork {
   constructor(rules = {}, task, number) {
@@ -11,47 +12,57 @@ class TestWork {
 
   [basicTask]() {
     let array = [];
-    switch (this.rules.fizzbuzz) {
-      case "all":
-        for (let i = 1; i <= this.number; i++) {
-          let word;
-          if (i % 5 === 0 && i % 3 === 0) {
-            word = "FizzBuzz";
-          } else if (i % 3 === 0) {
-            word = "Fizz";
-          } else if (i % 5 == 0) {
-            word = "Buzz";
-          } else {
-            word = i.toString();
-          }
-          array.push(word);
-        }
-        return array;
-      case "fizzbuzz":
-        for (let i = 1; i <= this.number; i++) {
-          let word;
-          if (i % 5 === 0 && i % 3 === 0) {
-            word = "FizzBuzz";
-          } else {
-            word = i.toString();
-          }
-          array.push(word);
-        }
-        return array;
-      default:
-        console.log("Default");
-        break;
+    for (let i = 1; i <= this.number; i++) {
+      let word;
+      if (i % 5 === 0 && i % 3 === 0) {
+        word = "FizzBuzz";
+      } else if (i % 3 === 0) {
+        word = "Fizz";
+      } else if (i % 5 === 0) {
+        word = "Buzz";
+      } else {
+        word = i.toString();
+      }
+      array.push(word);
     }
+    return array;
+  }
+
+  async [serverCall]() {
+    let array = [];
+    for (let i = 1; i <= this.number; i++) {
+      try {
+        const response = await fetch(`/fizz-buz-server/run?num=${i}`);
+        const data = await response.json();
+        array.push(data.solution);
+      } catch (error) {
+        console.error(error);
+        array.push(i.toString());
+      }
+    }
+    return array;
   }
 
   app() {
-    if (this.task === "basic") {
-      const result = this[basicTask]();
-      console.log(result);
-      return;
+    switch (this.task) {
+      case basicTask:
+        const basicResult = this[basicTask]();
+        console.log(basicResult);
+        break;
+      case serverCall:
+        this[serverCall]()
+          .then((serverResult) => {
+            console.log(serverResult);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+        break;
+      default:
+        console.log("Invalid task!");
     }
   }
 }
 
-const app = new TestWork({ fizzbuzz: "fizzbuzz" }, "basic", 15);
+const app = new TestWork({ fizzbuzz: "all" }, serverCall, 15);
 app.app();
